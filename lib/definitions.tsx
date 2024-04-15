902e6ab36e38093b9a6862cb1b0b0b7431e1f538
Felipe Gomes <contato@felipe-gomes.com>
2024-04-15 15:03:46 -0300

    feat: Create ProductCard, ProductCategory, and ProductTag interfaces

--- lib/definitions.tsx
+++ lib/definitions.tsx
@@ -7,3 +7,26 @@ interface Product {
   content: string;
   status: string;
 }
+
+interface ProductCard {
+  id: string;
+  slug: string;
+  title: string;
+  excerpt: string;
+  featuredImage: string;
+  category: ProductCategory["slug"];
+  featured: boolean;
+  qualities: ProductCategory[];
+}
+
+interface ProductCategory {
+  slug: string;
+  id: string;
+  name: string;
+}
+
+interface ProductTag {
+  slug: string;
+  id: string;
+  name: string;
+}
