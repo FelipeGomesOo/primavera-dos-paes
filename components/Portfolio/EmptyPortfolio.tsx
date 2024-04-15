25476b03456cd16cae8a7a54523c1642ab66b4d8
Felipe Gomes <contato@felipe-gomes.com>
2024-04-15 15:07:09 -0300

    feat: Add empty state for portfolio

---
+++ components/Portfolio/EmptyPortfolio.tsx
@@ -0,0 +1,38 @@
+import { getCategoryBySlug } from "@/lib/data";
+import Link from "next/link";
+export default async function EmptyPortfolio({
+  category,
+  products,
+}: {
+  category?: string;
+  products: ProductCard[];
+}) {
+  const categorySlug = category && category.length > 0 ? category : "";
+  const categoryName = category && (await getCategoryBySlug(category));
+  console.log(categoryName, "category name");
+  const categoryText = categoryName ? (
+    <>
+      <strong>{categoryName.name.toLowerCase()}</strong>
+    </>
+  ) : (
+    "produtos"
+  );
+
+  return (
+    <>
+      {products.length === 0 && (
+        <section className="col-span-12 py-10">
+          <h3 className="a">
+            Ops! Não encontramos {categoryText} com esse filtro.
+          </h3>
+          <Link
+            className="button medium mt-10"
+            href={`/produtos/${categorySlug}`}
+          >
+            Limpar filtros
+          </Link>
+        </section>
+      )}
+    </>
+  );
+}
