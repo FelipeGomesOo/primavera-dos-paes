664daf3ef33d2edbf0e4adcc6882c65c9dbef223
Felipe Gomes <contato@felipe-gomes.com>
2024-04-17 12:38:07 -0300

    feat: Add suspense to Tag

--- components/Portfolio/FilterByTag.tsx
+++ components/Portfolio/FilterByTag.tsx
@@ -1,9 +1,12 @@
 import Tag from "@/components/Portfolio/Tag";
+import { Suspense } from "react";
 export default function FilterByTag({ tags }: { tags: ProductTag[] }) {
   return (
     <>
       {tags.map((tag: any, index: number) => (
-        <Tag key={index} tagName={tag.name} tagSlug={tag.slug} />
+        <Suspense key={index} fallback={<>tag...</>}>
+          <Tag key={index} tagName={tag.name} tagSlug={tag.slug} />
+        </Suspense>
       ))}
     </>
   );
