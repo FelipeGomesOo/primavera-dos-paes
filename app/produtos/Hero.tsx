b11c6a5bda01f8b71b4a4b2146dec976f22ea797
Felipe Gomes <contato@felipe-gomes.com>
2024-04-17 12:35:03 -0300

    fix: Remove breadcrumb from hero

--- app/produtos/Hero.tsx
+++ app/produtos/Hero.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import Link from "next/link";
-
 export default function Hero({ children }: { children: React.ReactNode }) {
   return (
     <section className="container mb-4 md:mb-8 ">
