8456fa729ec9922e1087d5a82ff1d5d6414d1df2
Felipe Gomes <contato@felipe-gomes.com>
2024-04-24 08:21:05 -0300

    feat: Create logo separate part icons

---
+++ components/LogoIcon.tsx
@@ -0,0 +1,11 @@
+export default function LogoIcon({ className }: { className: string }) {
+  return (
+    <svg version="1.1" viewBox="0 0 42.6 59.7" className={className}>
+      <g id="bailarina">
+        <polygon id="grande" points="42.6,34.1 17,59.7 17,34.1 42.6,8.5 	" />
+        <polygon id="medio" points="25.5,8.5 17,17 17,8.5 25.5,0 	" />
+        <polygon id="pequeno" points="0,17 17,34.1 17,17 0,0 	" />
+      </g>{" "}
+    </svg>
+  );
+}
