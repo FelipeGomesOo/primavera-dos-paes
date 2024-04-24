a2d330790712051a27b442d463898b145469c31b
Felipe Gomes <contato@felipe-gomes.com>
2024-04-24 08:22:01 -0300

    chore: Store changes

--- components/Stores/Store.tsx
+++ components/Stores/Store.tsx
@@ -1,19 +1,87 @@
 import Image from "next/image";
+import Link from "next/link";
 export default function StoreCard({ store }: { store: Store }) {
+  console.log(store, "sotercard component");
+  const opening = [
+    {
+      nome: "Segunda",
+      horarios: "fechado",
+    },
+    {
+      nome: "Terça",
+      horarios: "11h-19h",
+    },
+    {
+      nome: "Quarta",
+      horarios: "11h-19h",
+    },
+    {
+      nome: "Quinta",
+      horarios: "11h-19h",
+    },
+    {
+      nome: "Sexta",
+      horarios: "11h-19h",
+    },
+    {
+      nome: "Sábado",
+      horarios: "11h-19h",
+    },
+    {
+      nome: "Domingo",
+      horarios: "fechado",
+    },
+  ];
   return (
-    <div className="w-6/12 lg:w-full flex flex-col lg:flex-row  gap-3 border border-primary-light/50  lg:items-center bg-[#fff]">
-      <div className="w-full lg:flex-none lg:w-3/12">
+    <div className="border border-primary-light/50 rounded min-h-[70svh] my-6">
+      <div className="w-full  bg-primary-light">
         <Image
           src={store.featuredImage.large.src}
           alt={store.featuredImage.large.alt}
           width={store.featuredImage.large.width}
           height={store.featuredImage.large.height}
-          className=""
+          className="h-48 w-full object-cover md:h-full "
         />
       </div>
-      <div className="grow p-2">
-        <h4>{store.Bairro}</h4>
-        <p>{store.Logradouro}</p>
+      <div className="w-full p-20 py-30 lg:self-center">
+        <div className="flex flex-col gap-4">
+          <div className="">
+            <h3>{store.Bairro}</h3>
+            <p>{store.Logradouro}</p>
+          </div>
+          <div className="w-full lg:flex gap-6">
+            <div className="w-full lg:w-6/12">
+              <h4 className="mb-2">Horários de abertura</h4>
+              {opening.map((day, index) => (
+                <div
+                  key={index}
+                  className="border-t text-sm border-primary-light/30 pt-1 flex flex-row gap-2 justify-between"
+                >
+                  <div className="w-6/12 md:w-8/12 ">{day.nome}</div>
+                  <div>{day.horarios}</div>
+                </div>
+              ))}
+            </div>
+            <div className="w-full lg:w-6/12">
+              <p>
+                A Fianco is an Italian vineria packed full of passion for
+                Italian wine and food. We choose wines that are farmed using the
+                old techniques, by small organic producers, with indigenous
+                grapes, respecting the land, respecting nature. We believe that
+                these old ways are the future.
+              </p>
+            </div>
+          </div>
+          <div className="w-full ">
+            <Link
+              target="_blank"
+              className="button medium"
+              href={store.MapsLink.url}
+            >
+              {store.MapsLink.title}
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
--- components/Stores/Stores.tsx
+++ components/Stores/Stores.tsx
@@ -1,16 +1,13 @@
-import StoreCard from "@/components/Stores/Store";
+import Store_2 from "@/components/Stores/Store_2";
 
 export default function Stores({ stores }: { stores: Store[] }) {
   return (
-    <section className="py-10 lg:pt-20 mt-20 bg-primary-light/10">
-      <div className="container mb-4">
-        <h3>Onde você encontra</h3>
-        <div className="flex flex-col lg:flex-row gap-6 lg:my-10 pt-10 lg:pt-0">
-          {stores.map((store: Store, index: number) => (
-            <StoreCard key={index} store={store} />
-          ))}
-        </div>
-      </div>
-    </section>
+    <div className="lg:container overflow-auto relative">
+      <section className="w-full px-[1rem] lg:px-0 snap-x snap-mandatory scrollable-content flex gap-4 lg:gap-6  overflow-x-auto lg:overflow-y-visible  items-center ">
+        {stores.map((store: Store, index: number) => (
+          <Store_2 key={index} store={store} />
+        ))}
+      </section>
+    </div>
   );
 }
