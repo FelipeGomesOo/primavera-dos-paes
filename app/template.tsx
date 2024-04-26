c218e6fa57afa5d01e592c6d5a641e7d1748b2f6
Felipe Gomes <contato@felipe-gomes.com>
2024-04-26 10:53:59 -0300

    feat: Make smooth scroll faster

--- app/template.tsx
+++ app/template.tsx
@@ -4,14 +4,5 @@ import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
 export default function Template({ children }: { children: React.ReactNode }) {
   const lenis = useLenis(({ scroll }) => {});
 
-  return (
-    <ReactLenis
-      root
-      options={{
-        duration: 3,
-      }}
-    >
-      {children}
-    </ReactLenis>
-  );
+  return <ReactLenis root>{children}</ReactLenis>;
 }
