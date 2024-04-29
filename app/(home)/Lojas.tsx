d97f8b800590b7ebb2ef5fe12c13e8157638f0e5
Felipe Gomes <contato@felipe-gomes.com>
2024-04-29 16:03:43 -0300

    feat: Improve store gallery mobile presentation

--- app/(home)/Lojas.tsx
+++ app/(home)/Lojas.tsx
@@ -7,32 +7,33 @@ export default async function Lojas() {
   const { lojas } = await getHomePage();
   return (
     <section id="lojas" className="bg-primary-light/20 py-[5svh] lg:py-[10svh]">
-      <div className="container">
-        <section className="border-t-4 pt-4 flex flex-col lg:flex-row justify-between gap-6 xg:gap-4 fade-in ">
-          <div className="w-full lg:w-6/12 lg:sticky top-10 h-[95svh]  lg:h-[90svh] flex flex-col justify-between">
-            <h2 className="fade-in">{lojas.titulo}</h2>
-            <div className="flex flex-col gap-6">
-              <div className="lg:hidden overflow-scroll">
-                <div className="mt-6 w-full flex gap-2">
-                  <StoresGallery stores={stores} />
+      <div className="lg:container">
+        <section className=" flex flex-col lg:flex-row justify-between gap-6 xg:gap-4 fade-in ">
+          <div className="px-4 lg:px-0 w-full lg:w-6/12">
+            <div className="pt-4 border-t-4  lg:sticky top-10 h-[85svh]  lg:h-[90svh] flex flex-col justify-between">
+              <h2 className="fade-in">{lojas.titulo}</h2>
+              <div className="flex flex-col gap-6">
+                <div className="text-sm md:text-base xl:pr-10">
+                  {stores.map((store: Store, index: number) => (
+                    <Store key={index} store={store} />
+                  ))}
                 </div>
-              </div>
-              <div className="text-sm md:text-base xl:pr-10">
-                {stores.map((store: Store, index: number) => (
-                  <Store key={index} store={store} />
-                ))}
-              </div>
-              <div className="text-sm md:text-base">
-                <h4 className="fade-in">{lojas.entregas.titulo}</h4>
-                <p className="xl:pr-10 fade-in">{lojas.entregas.descricao}</p>
-                <Link href={lojas.botao.link} className="mt-2 button small">
-                  {lojas.botao.titulo}
-                </Link>
+                <div className="text-sm md:text-base">
+                  <h4 className="fade-in">{lojas.entregas.titulo}</h4>
+                  <p className="xl:pr-10 fade-in">{lojas.entregas.descricao}</p>
+                  <Link href={lojas.botao.link} className="mt-2 button small">
+                    {lojas.botao.titulo}
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
-          <div className="lg:w-6/12 hidden lg:block">
-            <StoresGallery stores={stores} />
+          <div className="lg:w-6/12 ">
+            <div className="overflow-auto ">
+              <div className="mt-6 w-full flex gap-2 overflow-x-auto lg:overflow-x-none lg:block h-[50svh] lg:h-auto px-4 lg:px-0">
+                <StoresGallery stores={stores} />
+              </div>
+            </div>
           </div>
         </section>
       </div>
--- app/(home)/StoresGallery.tsx
+++ app/(home)/StoresGallery.tsx
@@ -5,7 +5,10 @@ export default function StoresGallery({ stores }: { stores: Store[] }) {
     <>
       {stores.map((store: Store, index: number) =>
         store.featuredImage.map((img: FaturedImage) => (
-          <div key={index + Math.random()} className="relative ">
+          <div
+            key={index}
+            className=" lg:mb-4  lg:max-h-auto lg:max-h-none scroll-ml-6 shrink-0 relative"
+          >
             <div className="absolute w-full h-40 bottom-0 text-sm left-0 px-1 flex flex-row items-end   z-10 text-[#fff] bg-gradient-to-t from-primary-dark/80 to-from-primary-dark/90 to-80%">
               <p className="px-2 py-2">
                 {store.Bairro}: {store.Logradouro}
@@ -16,7 +19,7 @@ export default function StoresGallery({ stores }: { stores: Store[] }) {
               alt={img.alt}
               width={img.width}
               height={img.height}
-              className="lg:w-full lg:mb-4 fade-in max-h-[30svh] lg:max-h-auto lg:max-h-none"
+              className="w-full h-full relative shrink-0 object-cover"
               placeholder="blur"
               blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mN8UQ8AAlUBaWcVN3EAAAAASUVORK5CYII="
               sizes="50vw"
